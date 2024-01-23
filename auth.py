798a43a85aeafa3415f681a2a2b9f07fa5c58c80
Neelam Karki <neelam.karki@wiseyak.com>
2024-01-23 12:28:31 +0545

    Simple google oauth implemented

---
+++ auth.py
@@ -0,0 +1,75 @@
+import datetime
+from starlette.requests import Request
+from fastapi import Response
+from starlette.responses import JSONResponse
+from fastapi import APIRouter, Depends
+from authlib.integrations.starlette_client import OAuthError
+from authlib.integrations.starlette_client import OAuth
+
+CLIENT_ID = '663515348764-tjaudng253e4ogmu9k1hoo59knmg90bq.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+REDIRECT_URI = 'http://localhost:8001/v1/auth'
+REDIRECT_FRONTEND = 'http://localhost:3000'
+
+
+oauth = OAuth()
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    client_kwargs={
+        'scope': 'openid profile email',
+        'redirect_uri': REDIRECT_URI
+    }  
+)
+
+
+router = APIRouter()
+
+
+
+@router.get("/login")
+async def login(request: Request):
+    url = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, url)
+
+
+@router.get('/auth')
+async def auth(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as e:
+        print(f"OAuthError: {e}")
+        return JSONResponse(content={"message": "couldn't connect"})
+
+    user_info = token.get('userinfo')
+
+    if user_info:
+        email = user_info.get('email')
+        name = user_info.get('name')
+
+        request.session['user'] = user_info
+        request.session['email'] = email
+        expires = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        request.session['access_token'] = token['access_token']
+        request.session['access_token_expires'] = expires.timestamp()
+
+        refresh_token_expires_in = token.get('expires_in')
+        if refresh_token_expires_in:
+            refresh_token_expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=refresh_token_expires_in)
+        else:
+            refresh_token_expires = datetime.datetime.utcnow() + datetime.timedelta(days=7)
+
+        request.session['refresh_token'] = token.get('refresh_token')
+        request.session['refresh_token_expires'] = refresh_token_expires.timestamp()
+
+
+    return JSONResponse({"user_info": user_info})
+
+
+@router.delete('/logout')
+def logout(request: Request):
+    request.session.clear()
+    return Response(status_code=200)
+
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-
+from starlette.middleware.sessions import SessionMiddleware
+import auth
 
 app = FastAPI()
 
@@ -17,8 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
+app.include_router(auth.router)
+app.add_middleware(SessionMiddleware, secret_key='qrdssrf2fgt')
 
 @app.get("/api")
 async def autocomplete():
